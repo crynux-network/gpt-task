eb1cacbe1ee85b29471afa1aaff80fccb3a7dfe3
iwehf <henry.lee@crynux.io>
2026-07-30 20:00:19 +0800

    fix. export to public interface

--- src/gpt_task/inference/__init__.py
+++ src/gpt_task/inference/__init__.py
@@ -1,4 +1,5 @@
 from .executed_gpu_count import get_executed_gpu_count
 from .inference import run_task
+from .tp.executor import shutdown_tp_executor
 
-__all__ = ["get_executed_gpu_count", "run_task"]
+__all__ = ["get_executed_gpu_count", "run_task", "shutdown_tp_executor"]
